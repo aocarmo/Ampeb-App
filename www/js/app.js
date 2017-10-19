44115c2af2e9cab1358a1384559be745f727c3c4
Alex Oliveira Carmo <alex.carmo91@gmail.com>
2017-10-19 00:46:03 -0300

    Correção de falhas:
    1 - Corrigida falha que não alertava ao usuário quando o login era expirado, esta falha causava problemas em Noticias, Fique por dentro, Eventos e Enquete, não exibindo o conteúdo dessas telas.

--- www/js/app.js
+++ www/js/app.js
@@ -100,8 +100,8 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
 
               //Pegando usuar803io e senha da sessão para obter um novo token
               var alertPopup = $ionicPopup.alert({
-                title: 'Ocorreu um erro inesperado',
-                template: 'Favor, entre contado com a administração do aplicativo.',
+                title: 'Ocorreu um erro, entre contado com a administração do aplicativo',
+                template: JSON.stringify(rejection),
                 okText: 'Ok', // String (default: 'OK'). The text of the OK button.
                 okType: 'button-assertive', // String (default: 'button-positive'). The type of the OK button.
               });
