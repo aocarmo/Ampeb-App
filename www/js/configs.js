aa2b83a1144a13d5dfb6ebaec2003993abe3844d
Alex Oliveira Carmo <alex.carmo91@gmail.com>
2018-05-11 13:15:49 -0300

    Iniciado o desenvolvimento do chat

---
+++ www/js/configs.js
@@ -0,0 +1,6 @@
+angular.module('Chat.configs', [])
+
+.constant("CONFIG", {
+  // "FIREBASE_URL": 'https://ionic-real-chat.firebaseio.com'
+  "FIREBASE_URL": 'https://ionic-v1-firebasechat.firebaseio.com'
+});
---
+++ www/js/init_firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDQWfXCG8vBYZRPliwLO4z7sLJoqcHqnC4",
+    authDomain: "ionic-v1-firebasechat.firebaseapp.com",
+    databaseURL: "https://ionic-v1-firebasechat.firebaseio.com",
+    projectId: "ionic-v1-firebasechat",
+    storageBucket: "",
+    messagingSenderId: "1030296434714"
+  };
+  
+  
+  firebase.initializeApp(firebaseConfig);
+  
